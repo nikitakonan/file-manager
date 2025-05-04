3767878dd08a22e3842b7d227e986189173c251f
nikitakonan <nikitakonan1991@gmail.com>
2025-05-04 22:57:49 +0400

    Added catch errors block

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-import { parseArgs } from "./src/args.js";
+import { homedir } from 'node:os';
+import { parseArgs } from './src/args.js';
 
 const args = parseArgs();
 
-const username = args.username ?? "anonymous";
+const username = args.username ?? 'anonymous';
+
+const currentDirectory = homedir();
 
 process.stdout.write(`Welcome to the File Manager, ${username}!\n`);
 printCurrentDirectory();
 
-process.stdin.on("data", (chunk) => {
-	const command = chunk.toString().trim();
+const commandFactory = {
+  exit: () => {
+    beforeExit();
+    process.exit();
+  },
+};
 
-	if (command === "exit") {
-		beforeExit();
-		process.exit();
-	}
+process.stdin.on('data', (chunk) => {
+  const command = chunk.toString().trim();
+
+  const fn = commandFactory[command];
+
+  if (typeof fn === 'function') {
+    try {
+      fn();
+    } catch (error) {
+      process.stdout.write('Operation failed\n');
+    }
+  } else {
+    // Invalid input
+    process.stdout.write('Invalid input\n');
+  }
 });
 
-process.on("SIGINT", () => {
-	beforeExit();
-	process.exit();
+process.on('SIGINT', () => {
+  beforeExit();
+  process.exit();
 });
 
 function beforeExit() {
-	process.stdout.write(
-		`Thank you for using File Manager, ${username}, goodbye!\n`,
-	);
+  process.stdout.write(
+    `Thank you for using File Manager, ${username}, goodbye!\n`
+  );
 }
 
 function printCurrentDirectory() {
-	process.stdout.write(`You are currently in  ${import.meta.dirname}\n`);
+  process.stdout.write(`You are currently in  ${currentDirectory}\n`);
 }
