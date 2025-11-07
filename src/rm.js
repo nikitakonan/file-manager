f0fc7f838ef595dc3ce64c0aa6822c6cae44d387
nikitakonan <nikitakonan1991@gmail.com>
2025-11-07 16:13:56 +0400

    fixture: relative or absolute path can be used in rm operation

--- src/rm.js
+++ src/rm.js
@@ -1,14 +1,14 @@
 import { unlink } from 'node:fs/promises';
-import { join } from 'node:path';
+import { isAbsolute, normalize, resolve } from 'node:path';
 
 export default async function rm(args, ctx) {
   if (args.length < 1) {
     throw new Error('Invalid input');
   }
 
-  const [fileName] = args;
+  const [path] = args;
 
-  const path = join(ctx.currentDirectory, fileName);
+  const fullPath = isAbsolute(path) ? normalize(path) : resolve(ctx.currentDirectory, path);
 
-  await unlink(path);
+  await unlink(fullPath);
 }
